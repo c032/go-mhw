4ee6e585b9fbf58c96041995bc0547971d601091
c032 <c032+github@wark.io>
2021-07-31 14:56:00 +0000

    List of small monsters

--- search.go
+++ search.go
@@ -20,6 +20,31 @@ const (
 
 func Monsters() ([]Creature, error) {
 	var (
+		err      error
+		monsters []Creature
+
+		tmp []Creature
+	)
+
+	tmp, err = LargeMonsters()
+	if err != nil {
+		return nil, fmt.Errorf("could not fetch large monsters: %w", err)
+	}
+
+	monsters = append(monsters, tmp...)
+
+	tmp, err = SmallMonsters()
+	if err != nil {
+		return nil, fmt.Errorf("could not fetch small monsters: %w", err)
+	}
+
+	monsters = append(monsters, tmp...)
+
+	return monsters, nil
+}
+
+func LargeMonsters() ([]Creature, error) {
+	var (
 		err error
 
 		currentURL *url.URL
@@ -33,7 +58,7 @@ func Monsters() ([]Creature, error) {
 		return nil, fmt.Errorf("could not parse url: %w", err)
 	}
 
-	req, err = http.NewRequest(http.MethodGet, largeMonstersURLStr, nil)
+	req, err = http.NewRequest(http.MethodGet, currentURL.String(), nil)
 	if err != nil {
 		return nil, fmt.Errorf("could not create request: %w", err)
 	}
@@ -50,9 +75,80 @@ func Monsters() ([]Creature, error) {
 		return nil, fmt.Errorf("could not parse response: %w", err)
 	}
 
+	var monsters []Creature
+
 	containerSelection := doc.Find(`#tagged-pages-container`)
 
+	containerSelection.Find(`a.wiki_link`).Each(func(i int, link *goquery.Selection) {
+		monsterName := link.Text()
+		monsterName = strings.TrimSpace(monsterName)
+		if monsterName == "" {
+			return
+		}
+
+		href := link.AttrOr("href", "")
+		href = strings.TrimSpace(href)
+		if href == "" {
+			return
+		}
+
+		var (
+			err        error
+			monsterURL *url.URL
+		)
+
+		monsterURL, err = currentURL.Parse(href)
+		if err != nil {
+			// TODO: Log.
+
+			return
+		}
+
+		monsters = append(monsters, &LargeMonster{
+			name:   monsterName,
+			rawURL: monsterURL.String(),
+		})
+	})
+
+	return monsters, nil
+}
+
+func SmallMonsters() ([]Creature, error) {
+	var (
+		err error
+
+		currentURL *url.URL
+
+		req  *http.Request
+		resp *http.Response
+	)
+
+	currentURL, err = url.Parse(smallMonstersURLStr)
+	if err != nil {
+		return nil, fmt.Errorf("could not parse url: %w", err)
+	}
+
+	req, err = http.NewRequest(http.MethodGet, currentURL.String(), nil)
+	if err != nil {
+		return nil, fmt.Errorf("could not create request: %w", err)
+	}
+
+	c := http.DefaultClient
+	resp, err = c.Do(req)
+	if err != nil {
+	}
+	defer resp.Body.Close()
+
+	var doc *goquery.Document
+	doc, err = goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("could not parse response: %w", err)
+	}
+
 	var monsters []Creature
+
+	containerSelection := doc.Find(`#tagged-pages-container`)
+
 	containerSelection.Find(`a.wiki_link`).Each(func(i int, link *goquery.Selection) {
 		monsterName := link.Text()
 		monsterName = strings.TrimSpace(monsterName)
--- search_test.go
+++ search_test.go
@@ -7,6 +7,46 @@ import (
 	"github.com/c032/go-mhw"
 )
 
+func TestLargeMonsters(t *testing.T) {
+	monsters, err := mhw.LargeMonsters()
+	if err != nil {
+		t.Fatalf("LargeMonsters() returned an error: %s", err)
+	}
+
+	if got := len(monsters); got == 0 {
+		t.Errorf("len(LargeMonsters()) = %d; want non-zero", got)
+	} else {
+		for i, monster := range monsters {
+			if got := monster.Name(); strings.TrimSpace(got) == "" {
+				t.Errorf("LargeMonsters()[%d].Name() = %#v; want non-empty string", i, got)
+			}
+			if got := monster.RawURL(); strings.TrimSpace(got) == "" {
+				t.Errorf("LargeMonsters()[%d].RawURL() = %#v; want non-empty string", i, got)
+			}
+		}
+	}
+}
+
+func TestSmallMonsters(t *testing.T) {
+	monsters, err := mhw.SmallMonsters()
+	if err != nil {
+		t.Fatalf("SmallMonsters() returned an error: %s", err)
+	}
+
+	if got := len(monsters); got == 0 {
+		t.Errorf("len(SmallMonsters()) = %d; want non-zero", got)
+	} else {
+		for i, monster := range monsters {
+			if got := monster.Name(); strings.TrimSpace(got) == "" {
+				t.Errorf("SmallMonsters()[%d].Name() = %#v; want non-empty string", i, got)
+			}
+			if got := monster.RawURL(); strings.TrimSpace(got) == "" {
+				t.Errorf("SmallMonsters()[%d].RawURL() = %#v; want non-empty string", i, got)
+			}
+		}
+	}
+}
+
 func TestMonsters(t *testing.T) {
 	monsters, err := mhw.Monsters()
 	if err != nil {
